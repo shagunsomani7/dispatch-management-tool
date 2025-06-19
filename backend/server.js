e7aae56f344f5b4ce799ff362467863390509359
shagunsomani7 <shagunsomani7@gmail.com>
2025-06-19 16:00:44 +0530

    Update package

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ app.use(morgan('combined'));
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'http://192.168.29.193:3000'
+        'http://192.168.29.193:3000',
+        'https://dispatch-management-tool-frontend.onrender.com/'
     ],
     credentials: true
 }));
